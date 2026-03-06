0762ec3ae414ceffbb1a4abd239c43f1e99e62eb
edwin ramos <edwinramosrusso123@gmail.com>
2026-03-06 07:56:11 -0500

    listo

--- taller_1082894778.py
+++ taller_1082894778.py
@@ -14,3 +14,19 @@ contador = 1
 while contador <= 5:
     print("numero:" + str(contador))
     contador = contador + 1
+
+for i in range(1, 12, 3):
+        print("numero for:" + str(i))
+
+encontrado = False
+numerobuscado = 12
+numeros = [1, 3, 5, 7, 9]
+for numero in numeros:
+    if numero == numerobuscado:
+        encontrado = True
+        break
+print("numero" , numerobuscado, "encontrado:", encontrado)   
+
+for i in range(3):
+    for j in range(3):
+        print("i:"+ str(i), "j:", str(j))
